b5f986dee83866043b52fc3bbd601f4611fac878
AlessandroArduino95 <alessandro.arduino@studenti.unipd.it>
2021-05-26 13:31:37 +0200

    Create scratch.py
    Python code added

---
+++ scratch.py
@@ -0,0 +1,11 @@
+# CODE FOR ADVANCE TOPICS
+
+def create_simple_mesh(x, y, s):
+    """Function used to create a simple mesh of a squared domain"""
+    return ()
+
+
+def ph_calc(nds, top, sv):
+    """Function used to assembly the P and H matrices element wise using the elements in
+    top and the nodes in nds. The parameter sv is the vector containing the S values for each element"""
+    return()
