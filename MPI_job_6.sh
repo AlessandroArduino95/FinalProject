16179f97e1cd60bd618bda550f52d12f9947d2a3
AlessandroArduino95 <alessandro.arduino@studenti.unipd.it>
2021-06-04 19:00:56 +0200

    Added slurm jobs and updated some files

--- MPI_job_6.sh
+++ MPI_job_6.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
-#SBATCH --job-name serialPDE
+#SBATCH --job-name PDE_MPI_6
 #SBATCH --error err_%j.txt
 #SBATCH --output out_%j.txt
 #SBATCH --mail-user alessandroarduino95@gmail.com
 #SBATCH --mail-type BEGIN,END,FAIL
 #SBATCH --partition allgroups
-#SBATCH --ntasks 1
-#SBATCH --mem 30M
+#SBATCH --ntasks 6
+#SBATCH --mem 1G
 #SBATCH --time 00:10:00
+
+spack load openmpi@3.1.4
 
 cd $SLURM_SUBMIT_DIR
 
-srun singularity exec fpcont.sif python3 script.py
+singularity exec tflow_opencv.sif \
+mpirun -np 6 python3 -m mpi4py 
